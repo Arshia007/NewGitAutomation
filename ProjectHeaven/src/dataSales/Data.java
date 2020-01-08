bf4547569b568f046a282a4a822a6d9f82a25a9f
faeqf <faeqf@192.168.1.50>
2020-01-08 17:41:56 -0500

    first initial chekcin

---
+++ ProjectHeaven/src/dataSales/Data.java
@@ -0,0 +1,12 @@
+package dataSales;
+
+public class Data {
+	
+	int Data [] = {0, 4, 24};
+	
+	public void display() {
+		
+		System.out.println("Data is displayed!");
+	}
+
+}
---
+++ ProjectHeaven/src/dataSales/Main.java
@@ -0,0 +1,19 @@
+package dataSales;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Data data = new Data();
+		displayGreeting();
+		data.display();
+		
+
+	}
+
+	private static void displayGreeting() {
+		System.out.println("Hello!");
+		
+	}
+
+}
